d18ed233550af14e4b7aaa0433d2e0a39658f1af
Abhishek Goyal <abhishek252167@gmail.com>
2020-06-06 21:48:28 +0530

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 # Author: Abhishek Goyal (abhishek252167@gmail.com)
-# Default is "World"
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
